5d15b0e124d0c71669e4d5c073222db7e4e706ab
desktop <finemini2001@gmail.com>
2025-03-27 00:52:13 +0900

    initialization

---
+++ springboot/src/main/java/skku/gymbarofit/GymbarofitApplication.java
@@ -0,0 +1,13 @@
+package skku.gymbarofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymbarofitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymbarofitApplication.class, args);
+	}
+
+}
---
+++ springboot/src/test/java/skku/gymbarofit/GymbarofitApplicationTests.java
@@ -0,0 +1,13 @@
+package skku.gymbarofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymbarofitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
